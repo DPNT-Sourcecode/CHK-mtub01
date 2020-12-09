8c038cb2ede641f5059a2d4b32ae05e78fa309d6
sbx_user1051 <sbx_user1051@169.254.2.245>
2020-12-09 11:49:17 +0000

    Wed Dec 09 11:49:17 UTC 2020

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -2,10 +2,10 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+const checkout = require('../../../lib/solutions/CHK/checkout');
 
-describe('SUM challenge: adding two numbers', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
+describe('Checkout Challenge: calculate checkout amount', function() {
+	it('should return -1, if empty string or null', function() {
+	    assert.equal(checkout(''), -1);
 	});
 });
