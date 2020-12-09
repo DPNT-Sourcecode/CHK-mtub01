275816f8afe27a69b94dd267102797c228781e47
sbx_user1051 <sbx_user1051@169.254.2.245>
2020-12-09 11:52:17 +0000

    Wed Dec 09 11:52:17 UTC 2020

---
+++ test/solutions/CHK/test-products.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const checkout = require('../../../lib/solutions/CHK/checkout');
+
+describe('Checkout Challenge: calculate checkout amount', function() {
+	it('should return -1, if empty string or null', function() {
+	    assert.equal(checkout(''), -1);
+	});
+});
