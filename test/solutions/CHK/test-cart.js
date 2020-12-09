450b4ff38caabbe3dab05e4c2328fc86ade4ea40
sbx_user1051 <sbx_user1051@169.254.5.109>
2020-12-09 15:03:19 +0000

    Wed Dec 09 15:03:19 UTC 2020

--- test/solutions/CHK/test-cart.js
+++ test/solutions/CHK/test-cart.js
@@ -4,8 +4,11 @@ var it = mocha.it
 var assert = require('assert');
 const cart = require('../../../lib/solutions/CHK/cart');
 
-describe('Checkout Challenge: calculate checkout amount', function() {
-	it('should return 0, if empty string or null', function() {
+describe('Checkout Challenge: add to cart', function() {
+	it('should return 1 for SKU A', function() {
+		const product={sku:'A',price:'50'}
+		const skulist='A'
+		const cartObj=new cart.Cart()
 	    assert.equal(checkout(''), 0);
 	});
 });
