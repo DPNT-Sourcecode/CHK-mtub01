7db1c939d12b137fe3f006c2f7cdadf715ab2729
sbx_user1051 <sbx_user1051@169.254.5.109>
2020-12-09 15:06:19 +0000

    Wed Dec 09 15:06:19 UTC 2020

--- test/solutions/CHK/test-cart.js
+++ test/solutions/CHK/test-cart.js
@@ -7,8 +7,11 @@ const cart = require('../../../lib/solutions/CHK/cart');
 describe('Checkout Challenge: add to cart', function() {
 	it('should return 1 for SKU A', function() {
 		const product={sku:'A',price:'50'}
-		const skulist='A'
 		const cartObj=new cart.Cart()
-	    assert.equal(checkout(''), 0);
+		cartObj.addToCart(product)
+		const productsInCart=cartObj.getCartItems()
+		assert.equal(productsInCart.length, 1);
+		assert.equal(productsInCart[0].quantity, 1);
+		assert.equal(productsInCart[0].product.sku, product.sku);
 	});
 });
