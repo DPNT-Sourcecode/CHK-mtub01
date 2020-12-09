b661ecc5e39ad1fb5a50c40844d8a7e31e3f3397
sbx_user1051 <sbx_user1051@169.254.2.245>
2020-12-09 12:04:17 +0000

    Wed Dec 09 12:04:17 UTC 2020

--- test/solutions/CHK/test-products.js
+++ test/solutions/CHK/test-products.js
@@ -5,8 +5,13 @@ var assert = require('assert');
 const products = require('../../../lib/solutions/CHK/products');
 
 describe('Checkout Challenge: Get Product List', function() {
-	it('should return all products', function() {
-		const productDetails=products.all()
-	    assert.equal(productDetails.length, 4);
+	it('should be able to add products', function() {
+		const productList=new products.Products()
+		productList.add('newProduct')
+		assert.equal(productList.products[0], 'newProduct');
 	});
+	// it('should return all products', function() {
+	// 	const productDetails=products.all()
+	//     assert.equal(productDetails.length, 4);
+	// });
 });
