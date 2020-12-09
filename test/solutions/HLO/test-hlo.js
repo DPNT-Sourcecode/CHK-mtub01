a33f3fa6a15ce82bcb6b05b76eb47f559ebd9256
sbx_user1051 <sbx_user1051@169.254.17.133>
2020-12-09 11:27:53 +0000

    Wed Dec 09 11:27:53 UTC 2020

--- test/solutions/HLO/test-hlo.js
+++ test/solutions/HLO/test-hlo.js
@@ -2,10 +2,10 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+const hello = require('../../../lib/solutions/HLO/hello');
 
-describe('SUM challenge: adding two numbers', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
+describe('HLO challenge: say Hello World', function() {
+	it('should return Hello World', function() {
+	    assert.equal(hello(), "Hello World");
 	});
 });
